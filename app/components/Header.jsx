5b1f1218c22f8504c3d470082968310e0fe0189d
Rebec R <rebecaruni7@gmail.com>
2024-03-07 16:30:22 -0600

    Added components for landing page

---
+++ app/components/Header.jsx
@@ -0,0 +1,9 @@
+import "app/styles/header.css";
+
+export default function Header() {
+  return (
+    <div className="header-component">
+      <div className="header">WCOA Logo</div>
+    </div>
+  );
+}
