f4ca48d95ffdee26a0e8bd60de4e695be78f44d7
CalvinVan <cvanbusiness@gmail.com>
2024-03-07 16:51:16 -0600

    Added Logo for header component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "app/styles/header.css";
+
+export default function Footer() {
+  return (
+    <div className="header-component">
+      <div className="header">This should be a footer</div>
+    </div>
+  );
+}
