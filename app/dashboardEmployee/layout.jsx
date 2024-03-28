d3d0fd7211a8c336b86d09203d71c31c5724758c
Rebec R <rebecaruni7@gmail.com>
2024-03-28 16:02:22 -0500

    Navigation menu without styling

---
+++ app/dashboardEmployee/layout.jsx
@@ -0,0 +1,13 @@
+import SideNav from "@/app/ui/dashboardEmployee/sidenav";
+export default function DashboardLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
---
+++ app/ui/dashboardEmployee/nav-links.jsx
@@ -0,0 +1,35 @@
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: "Dashboard", href: "/dashboardEmployee" },
+  {
+    name: "Rides",
+    href: "/dashboardEmployee/rides",
+  },
+  {
+    name: "Volunteers",
+    href: "/dashboardEmployee/volunteers",
+  },
+  {
+    name: "Settings",
+    href: "/dashboardEmployee/settings",
+  },
+];
+
+export default function NavLinks() {
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <a
+            key={link.name}
+            href={link.href}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          >
+            <p className="hidden md:block">{link.name}</p>
+          </a>
+        );
+      })}
+    </>
+  );
+}
