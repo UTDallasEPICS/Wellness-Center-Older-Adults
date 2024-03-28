03a74f910641012b9a506952015bb1575baa9d69
4hmedSheik <144933612+4hmedSheik@users.noreply.github.com>
2024-03-28 16:01:12 -0500

    Added footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,20 @@
-import "app/styles/header.css";
+// Import footer
+import React from 'react';
+import 'app/styles/footer.css'; // Importing CSS (style) file for Footer
 
-export default function Footer() {
-  return (
-    <div className="header-component">
-      <div className="header">This should be a footer</div>
-    </div>
-  );
-}
+const Footer = () => (
+  <footer className="footer-container">
+    <h2>Wellness Center for Older Adults</h2>
+    <p className="address">
+      401 W 16th Street STE 600, Plano, TX 75075
+    </p>
+    <p className="contact">
+      972-953-7669 | FAX 972-970-2040
+    </p>
+    <p className="Location">
+      (Located inside the Sam Johnson Senior Rec Center)
+    </p>
+  </footer>
+);
+
+export default Footer;
