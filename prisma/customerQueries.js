9902040c37cb26f73515d5db5ae124989cd95dbe
AvishiktaBagchi <avi.bagchi04@gmail.com>
2024-04-25 15:38:11 -0500

    edited customer model and updated customerQuery based on the new model

--- prisma/customerQueries.js
+++ prisma/customerQueries.js
@@ -1,5 +1,6 @@
 /*
 Author: Avishikta Bagchi (3/4/2024)
+Updated: 4/18/2024
 Checks for a customer using their email, first name, and last name.
 Creates a new user if one doesn't exist in the database.
 */
@@ -9,15 +10,15 @@ const prisma = new PrismaClient();
 async function customerQueries()
 {
 const customerEmail = 'dummyCustomer@gmail.com'
-const customerFirstName = 'dummyFName'
-const customerLastName = 'dummyLName'
+const customerFName = 'dummyFName'
+const customerLName = 'dummyLName'
 
 const customer = await prisma.customer.findMany({
     where: {
       user: {
         email: customerEmail,
-        firstName: customerFirstName,
-        lastName: customerLastName
+        firstName: customerFName,
+        lastName: customerLName
       }
     },
     include: {
@@ -33,8 +34,8 @@ if(!customer[0])
           user: {
             create: {
               email: customerEmail,
-              firstName: customerFirstName,
-              lastName: customerLastName,
+              firstName: customerFName,
+              lastName: customerLName,
               phone: "000000000",
             }
           }
