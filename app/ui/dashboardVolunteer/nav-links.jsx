4afe4b052a92400176be2c27593aeaedd4c730ad
Rebec R <rebecaruni7@gmail.com>
2024-05-02 15:48:41 -0500

    Added Volunteer Site (navbar and pages for volunteer side of web app)

---
+++ app/ui/dashboardVolunteer/nav-links.jsx
@@ -0,0 +1,50 @@
+"use client";
+import "app/styles/sideNav.css";
+import "app/globalicons.css";
+import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { name: "Dashboard", href: "/dashboardVolunteer" }, // Using icon name from Material Symbols Rounded font
+
+  {
+    name: "Rides",
+    href: "/dashboardVolunteer/rides",
+  },
+  {
+    name: "Hours",
+    href: "/dashboardVolunteer/hours",
+  },
+
+  {
+    name: "Settings",
+    href: "/dashboardVolunteer/settings",
+  },
+];
+
+export default function NavLinks() {
+  const [activeLink, setActiveLink] = useState(null);
+  const pathname = usePathname();
+
+  const handleClick = (index) => {
+    setActiveLink(index);
+  };
+
+  return (
+    <>
+      {links.map((link, index) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={index === activeLink ? "nav-a active" : "nav-a"}
+            onClick={() => handleClick(index)}
+          >
+            <p className="nav-p">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
