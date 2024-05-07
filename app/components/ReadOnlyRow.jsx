27d2a9d3da9102b03b3e3648dd851fe8b8b37045
Rebec R <rebecaruni7@gmail.com>
2024-05-07 17:52:22 -0500

    Changed ride table to have icons

--- app/components/ReadOnlyRow.jsx
+++ app/components/ReadOnlyRow.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const ReadOnlyRow = ({contact, handleEditClick, handleDeleteClick}) => {
-  return(
-
-      <tr>
-        <td>{contact.clientName}</td>
-        <td>{contact.phoneNumber}</td>
-        <td>{contact.address}</td>
-        <td>{contact.startTime}</td>
-        <td>
-          <button type="button" onClick={(event)=> handleEditClick(event, contact)}>
-            Edit
-          </button>
-          <button type="button" onClick={()=>handleDeleteClick(contact.id)}>Delete</button>
-        </td>
-      </tr>
-  
+const ReadOnlyRow = ({ contact, handleEditClick, handleDeleteClick }) => {
+  return (
+    <tr>
+      <td>{contact.clientName}</td>
+      <td>{contact.phoneNumber}</td>
+      <td>{contact.address}</td>
+      <td>{contact.startTime}</td>
+      <td>
+        <button
+          type="button"
+          onClick={(event) => handleEditClick(event, contact)}
+        >
+          <span class="material-symbols-rounded">edit</span>
+        </button>
+        <button type="button" onClick={() => handleDeleteClick(contact.id)}>
+          <span class="material-symbols-rounded">delete</span>
+        </button>
+      </td>
+    </tr>
   );
 };
 
