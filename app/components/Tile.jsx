bdf65272c6c216883c6dde7a975466c2934f2b11
4hmedSheik <144933612+4hmedSheik@users.noreply.github.com>
2024-05-07 15:36:36 -0500

    Added Rides Reserved tab as well as a successfully added message as well as unsuccessful

---
+++ app/components/Tile.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'app/styles/tile.css'; 
+
+const Tile = () => {
+  return (
+    <div className="tile">
+      <table className="summary-table">
+        <thead>
+          <tr>
+            <th>Volunteer Name</th>
+            <th>Contact</th>
+            <th>Address</th>
+            <th>Volunteer Hours</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            
+            <td>Jane Smith</td>
+            <td>123-456-6789</td>
+            <td>123 Address</td>
+            <td>5 hrs</td>
+          </tr>
+        </tbody>
+      </table>
+
+     
+      <div className="detail-info">
+        Name: Jane Smith<br />
+        Address: 123 Address<br />
+        Phone number: 321-654-1987<br />
+        Volunteers Hours: 5hrs
+      </div>
+    </div>
+  );
+};
+
+export default Tile;
---
+++ app/dummyTile/page.jsx
@@ -0,0 +1,7 @@
+import Tile from "app/components/Tile.jsx";
+
+export default function dummyTile() {
+    return <Tile/>
+    
+  }
+  
