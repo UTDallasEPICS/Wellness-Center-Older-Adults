1b04932cac239a4b735aa187aa6450d9265e49b7
Rebec R <rebecaruni7@gmail.com>
2024-05-07 15:36:30 -0500

    Added signout popup

--- app/components/DashHeader.jsx
+++ app/components/DashHeader.jsx
@@ -1,19 +1,55 @@
+"use client";
+import { useState } from "react";
 import "app/styles/dashHeader.css";
 
 export default function DashHeader() {
+  const [display, setDisplay] = useState(false);
   return (
-    <div className="header-component">
-      <div className="logo-component">
-        <a href="/">
-          <p className="header-text">WCOA</p>
-        </a>
+    <div>
+      <div className="header-component">
+        <div className="logo-component">
+          <a href="/">
+            <p className="header-text">WCOA</p>
+          </a>
+        </div>
+        <div className="profile-circle">
+          <input
+            type="image"
+            className="profile-placeholder"
+            src="/images/profileImagePlaceHolder.png"
+            alt="profile image"
+            onClick={() => setDisplay((prevDisplay) => !prevDisplay)}
+          />
+        </div>
       </div>
-      <div className="profile-circle">
-        <img
-          className="profile-placeholder"
-          src="/images/profileImagePlaceHolder.png"
-          alt="profile image"
-        />
+      <div
+        className="logout-popup"
+        style={{ display: display ? "block" : "none" }}
+      >
+        <div className="popup-content">
+          <p>Logout</p>
+          <button
+            type="button"
+            className="changeAccountButton"
+            onClick={() => setDisplay((prevDisplay) => !prevDisplay)}
+          >
+            Change Account
+          </button>
+          <button
+            type="button"
+            className="logoutButton"
+            onClick={() => setDisplay((prevDisplay) => !prevDisplay)}
+          >
+            Sign Out
+          </button>
+        </div>
+        <button
+          type="button"
+          className="cancelButton"
+          onClick={() => setDisplay((prevDisplay) => !prevDisplay)}
+        >
+          <span class="material-symbols-rounded">close</span>
+        </button>
       </div>
     </div>
   );
