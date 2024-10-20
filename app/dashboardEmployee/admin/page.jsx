0aae69bd13a2a9897474a0c79ab2c40ccbb222c2
aaryaamoharir <66750249+aaryaamoharir@users.noreply.github.com>
2024-10-20 12:45:23 -0500

    pushed code to integrate admin registration form to the admin tab

--- app/dashboardEmployee/admin/page.jsx
+++ app/dashboardEmployee/admin/page.jsx
@@ -1,4 +1,16 @@
-export default function Page() {
-  return <p>Admin Page</p>;
-}
 
+import Image from 'next/image';
+import "/app/styles/volunteerAdmin.css"
+
+export default function Page() {
+  return (
+    <div className="page-component">
+      <div className="page">
+        <h1 className="page-title">Welcome to the Admin Portal</h1>
+        <a href="/registration">
+          <button className="register-button">Register Admin Here</button>
+        </a>
+      </div>
+    </div>
+  );
+}
