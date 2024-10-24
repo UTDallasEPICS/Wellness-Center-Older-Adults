16b930e2280c5c59ac4072471535567303733dd4
AtreyaG <atreyag05@gmail.com>
2024-10-24 15:20:51 -0500

    Fixed Typos

--- app/components/CancelRidesModel.jsx
+++ app/components/CancelRidesModel.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const CancelRidesModel = ({ handleConfirmDelete, handleCancelDelete }) => {
+const CancelRidesModel = ({ handleConfirmCancel, handleDenyCancel}) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
       <div className="bg-white p-6 rounded-lg shadow-lg w-[400px]">
-        <h2 className="text-lg font-semibold mb-4">Confirm Delete</h2>
-        <p className="mb-6">Are you sure you want to delete ride?</p>
+        <h2 className="text-lg font-semibold mb-4">Confirm Cancel</h2>
+        <p className="mb-6">Are you sure you want to cancel ride?</p>
         <div className="flex justify-end">
           <button
             type="button"
-            onClick={handleCancelDelete}
+            onClick={handleDenyCancel}
             className="bg-gray-600 text-white py-2 px-4 rounded-md mr-2"
           >
-            Cancel
+            No
           </button>
           <button
             type="button"
-            onClick={handleConfirmDelete}
+            onClick={handleConfirmCancel}
             className="bg-red-600 text-white py-2 px-4 rounded-md"
           >
-            Delete
+            Yes
           </button>
         </div>
       </div>
