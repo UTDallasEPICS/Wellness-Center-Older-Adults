416afdf0f03134f6fb9608ef84c14db3e9c73217
aaryaamoharir <66750249+aaryaamoharir@users.noreply.github.com>
2024-11-11 23:39:36 -0600

    fixed issues with dummy data

--- app/api/createCustomerAccount/route.ts
+++ app/api/createCustomerAccount/route.ts
@@ -49,6 +49,7 @@ export async function POST(req: Request) {
       streetAddress: 'dummy street address',
       city: 'dummy city',
       state: 'dummy state',
+      zipcode: '75024', 
       birthdate: new Date('2022-09-27T18:00:00.000Z').toISOString(),
     },
   });
