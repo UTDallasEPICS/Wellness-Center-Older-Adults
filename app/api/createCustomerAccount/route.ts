881cce8739e5c3a4b122fe6765a220476fcdc456
aaryaamoharir <66750249+aaryaamoharir@users.noreply.github.com>
2024-11-11 23:50:48 -0600

    changed dummy data

--- app/api/createCustomerAccount/route.ts
+++ app/api/createCustomerAccount/route.ts
@@ -41,15 +41,15 @@ export async function POST(req: Request) {
   //otherwise, a new customer is created
   await prisma.customer.create({
     data: {
-      customerEmail,
-      firstName,
-      middleName: 'dummy middle name',
-      lastName,
-      customerPhone: '0000000000',
-      streetAddress: 'dummy street address',
-      city: 'dummy city',
-      state: 'dummy state',
-      birthdate: new Date('2022-09-27T18:00:00.000Z').toISOString(),
+      customerEmail: customerEmail, // Ensure `customerEmail` is a string
+      firstName: firstName, // Ensure `firstName` is a string
+      middleName: 'dummy middle name', // Optional, but included here
+      lastName: lastName, // Ensure `lastName` is a string
+      customerPhone: '0000000000', // Ensure it's a string
+      streetAddress: 'dummy street address', // Ensure it's a string
+      city: 'dummy city', // Ensure it's a string
+      state: 'dummy state', // Optional, included here
+      birthdate: new Date('2022-09-27T18:00:00.000Z').toISOString(), // Optional, included here
     },
   });
 
