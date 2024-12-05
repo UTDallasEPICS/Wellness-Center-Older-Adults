1f2b1612b229c6aa00dc5b65a18612ad71e7532a
Arif Nizami <arif@ratiodating.app>
2024-12-05 16:32:48 -0600

    removed console

--- wait.mjs
+++ wait.mjs
@@ -1,8 +1,6 @@
 // wait.mjs
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 (async () => {
-  console.log('Sleeping for 2 seconds...');
-  await sleep(2000);  // Sleep for 2 seconds
-  console.log('Awoke after 2 seconds!');
+  await sleep(2000); // Sleep for 2 seconds
 })();
