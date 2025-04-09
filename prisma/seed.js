31ed4ef40010dafb1098ecfdfaf03f1a437481cb
Deethya0715 <djanjanam@gmail.com>
2025-04-09 16:12:30 -0500

    new address was added

--- prisma/seed.js
+++ prisma/seed.js
@@ -17,6 +17,30 @@ async function main() {
       phone: "2345678901",
       role: "VOLUNTEER",
     },
+    data:{
+   startaddress: {
+      street: "8209 Cottage Dr.",
+      city: "Mckinney",
+      state: "TX",
+      zip: "75070",
+    },
+    endaddress: {
+      street: "8921 Mount Ranier Dr.",
+      city: "Plano",
+      state: "TX",
+      zip: "75025",
+    },
+    pickUpTime: new Date("2023-10-01T10:00:00Z"),
+    date: new Date("2023-10-01"),
+    ways: 2,
+    extraInfo: "Help with groceries",
+    status: "Added",
+    volunteerName: "Volly Ball",
+    hours: 2,
+    time: new Date("2023-10-01T12:00:00Z"),
+    customerName: "Deethya Janjanam",
+    },
+
   });
 
   // Other Seed Data goes here
