bb36f1993f5aef534cc8e93e345d6586f3dd1982
TusharW4ni <reachtusharwani@gmail.com>
2025-04-30 18:17:26 -0500

    removed second header from volunteers layout

--- app/Dashboard/volunteers/layout.jsx
+++ app/Dashboard/volunteers/layout.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import SideNav from "@/app/components/Sidebar/sidenav";
 import React from "react";
 import { ToastContainer } from "react-toastify";
@@ -10,37 +10,45 @@ export default function DashboardLayout({ children }) {
 
   const handleResize = () => {
     if (typeof window !== "undefined" && window.innerWidth <= 768) {
-      setIsCollapsed(true); 
+      setIsCollapsed(true);
     } else {
-      setIsCollapsed(true); 
+      setIsCollapsed(true);
     }
   };
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       handleResize();
-      window.addEventListener('resize', handleResize);
+      window.addEventListener("resize", handleResize);
 
       return () => {
-        window.removeEventListener('resize', handleResize);
+        window.removeEventListener("resize", handleResize);
       };
     }
   }, []);
 
   return (
     <div className="flex flex-col w-full min-h-screen">
-      <div className="w-full h-[70px] border-b border-gray-300">
+      {/* <div className="w-full h-[70px] border-b border-gray-300">
         <a href="/Dashboard">
           <p className="p-5 text-lg font-light text-left text-gray-900 w-1/2">WCOA</p>
         </a>
-      </div>
+      </div> */}
 
       <div className="flex bg-white">
-        <SideNav toggleCollapse={() => setIsCollapsed(!isCollapsed)} isCollapsed={isCollapsed} />
-        <div 
+        <SideNav
+          toggleCollapse={() => setIsCollapsed(!isCollapsed)}
+          isCollapsed={isCollapsed}
+        />
+        <div
           className={`flex-1 h-[calc(100vh-70px)] transition-all ease-in-out duration-300 ${isCollapsed ? "ml-[70px]" : "ml-0"} md:${isCollapsed ? "ml-[70px]" : "ml-[250px]"}`}
           style={{
-            paddingLeft: typeof window !== "undefined" && window.innerWidth > 768 ? (isCollapsed ? '70px' : '250px') : '0',
+            paddingLeft:
+              typeof window !== "undefined" && window.innerWidth > 768
+                ? isCollapsed
+                  ? "70px"
+                  : "250px"
+                : "0",
           }}
         >
           {children}
