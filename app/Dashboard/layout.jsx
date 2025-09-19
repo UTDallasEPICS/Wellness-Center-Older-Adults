38cb082e5cff525638d60789207a27766f7a2c6b
TusharW4ni <reachtusharwani@gmail.com>
2025-09-18 20:56:05 -0500

    fixed dashboard top bar image

--- app/Dashboard/layout.jsx
+++ app/Dashboard/layout.jsx
@@ -1,34 +1,35 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import SideNav from "@/app/components/Sidebar/sidenav";
 import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Image from 'next/image';
+import Image from "next/image";
+import logo from "../../public/images/logo.png";
 
 export default function DashboardLayout({ children }) {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   const handleResize = () => {
     if (typeof window !== "undefined" && window.innerWidth <= 768) {
-      setIsCollapsed(true); 
+      setIsCollapsed(true);
     } else {
-      setIsCollapsed(true); 
+      setIsCollapsed(true);
     }
   };
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       handleResize();
-      window.addEventListener('resize', handleResize);
+      window.addEventListener("resize", handleResize);
 
       return () => {
-        window.removeEventListener('resize', handleResize);
+        window.removeEventListener("resize", handleResize);
       };
     }
   }, []);
 
-/*<div className="w-full h-[70px] bg-[#fffdf5] border-b border-gray-300">
+  /*<div className="w-full h-[70px] bg-[#fffdf5] border-b border-gray-300">
         <a href="/Dashboard">
        
         </a>
@@ -37,22 +38,30 @@ export default function DashboardLayout({ children }) {
     <div className="flex flex-col w-full min-h-screen">
       <div className="w-full h-[70px] bg-[#fffdf5] border-b border-gray-300">
         <a href="/Dashboard">
-        <Image 
-            className="w-60 h-auto mt-2" 
-            src="/logo.png" 
-            alt="Logo" 
-            width={1600} 
-            height={800} 
-          />    
+          <Image
+            className="w-60 h-auto mt-2"
+            src={logo}
+            alt="Logo"
+            width={1600}
+            height={800}
+          />
         </a>
       </div>
 
       <div className="flex bg-white">
-        <SideNav toggleCollapse={() => setIsCollapsed(!isCollapsed)} isCollapsed={isCollapsed} />
-        <div 
+        <SideNav
+          toggleCollapse={() => setIsCollapsed(!isCollapsed)}
+          isCollapsed={isCollapsed}
+        />
+        <div
           className={`flex-1 h-[calc(100vh-70px)] bg-[#fffdf5] transition-all ease-in-out duration-300 ${isCollapsed ? "ml-[70px]" : "ml-0"} md:${isCollapsed ? "ml-[70px]" : "ml-[250px]"}`}
           style={{
-            paddingLeft: typeof window !== "undefined" && window.innerWidth > 768 ? (isCollapsed ? '70px' : '250px') : '0',
+            paddingLeft:
+              typeof window !== "undefined" && window.innerWidth > 768
+                ? isCollapsed
+                  ? "70px"
+                  : "250px"
+                : "0",
           }}
         >
           {children}
--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,114 +1,137 @@
-import { NextResponse } from 'next/server';
-import { loginRedirectUrl, logoutRedirectUrl } from '../auth0';
-import { cookies } from 'next/headers';
-import prisma from '../../../../util/prisma-client';
-import { jwtVerify, importX509 } from 'jose';
+/* eslint-disable prettier/prettier */
+import { NextResponse } from "next/server";
+import { loginRedirectUrl, logoutRedirectUrl } from "../auth0";
+import { cookies } from "next/headers";
+import prisma from "../../../../util/prisma-client";
+import { jwtVerify, importX509 } from "jose";
 
-export async function GET(request: Request, { params }: { params: { auth0: string } }) {
-    const auth0 = params.auth0;
+export async function GET(
+  request: Request,
+  { params }: { params: { auth0: string } }
+) {
+  const auth0 = params.auth0;
 
-    if (auth0 === 'login') {
-        const url = loginRedirectUrl();
-        return NextResponse.json({ url });
-    } else if (auth0 === 'logout') {
-        const cookieStore = cookies();
-        const id_token = cookieStore.get('cvtoken')?.value;
-        const url = logoutRedirectUrl(id_token as string);
-        return NextResponse.json({ url });
-    } else if (auth0 === 'logout-complete') {
-        const ret = NextResponse.redirect('http://localhost:3000', { status: 302 });
-        ret.cookies.delete('cvtoken');
-        ret.cookies.delete('cvuser');
-        return ret;
-    } else if (auth0 === 'session') {
-        const cookieStore = cookies();
-        const id_token = cookieStore.get('cvtoken')?.value;
-        const isAuthenticated = !!id_token && id_token.trim() !== '';
-        return NextResponse.json({ isAuthenticated });
-    }
+  if (auth0 === "login") {
+    const url = loginRedirectUrl();
+    return NextResponse.json({ url });
+  } else if (auth0 === "logout") {
+    const cookieStore = cookies();
+    const id_token = cookieStore.get("cvtoken")?.value;
+    const url = logoutRedirectUrl(id_token as string);
+    return NextResponse.json({ url });
+  } else if (auth0 === "logout-complete") {
+    const ret = NextResponse.redirect("http://localhost:3000", { status: 302 });
+    ret.cookies.delete("cvtoken");
+    ret.cookies.delete("cvuser");
+    return ret;
+  } else if (auth0 === "session") {
+    const cookieStore = cookies();
+    const id_token = cookieStore.get("cvtoken")?.value;
+    const isAuthenticated = !!id_token && id_token.trim() !== "";
+    return NextResponse.json({ isAuthenticated });
+  }
 
-    return NextResponse.json({ error: 'Invalid auth0 action' }, { status: 400 });
+  return NextResponse.json({ error: "Invalid auth0 action" }, { status: 400 });
 }
-export async function POST(request: Request, { params }: { params: { auth0: string } }) {
-    const auth0 = params.auth0;
+export async function POST(
+  request: Request,
+  { params }: { params: { auth0: string } }
+) {
+  const auth0 = params.auth0;
 
-    if (auth0 === 'callback') {
-        try {
-            const requestBody = await request.text();
-            const formData = new URLSearchParams(requestBody);
-            const id_token = formData.get('id_token');
+  if (auth0 === "callback") {
+    try {
+      const requestBody = await request.text();
+      const formData = new URLSearchParams(requestBody);
+      const id_token = formData.get("id_token");
 
-            if (!id_token) {
-                console.error('Error: id_token is missing in the callback.');
-                return NextResponse.redirect('/?error=missing_id_token', { status: 302 });
-            }
+      if (!id_token) {
+        console.error("Error: id_token is missing in the callback.");
+        return NextResponse.redirect("/?error=missing_id_token", {
+          status: 302,
+        });
+      }
 
-            const certPem = process.env.CERT_PEM;
-            if (!certPem) {
-                console.error('Error: CERT_PEM environment variable not found.');
-                return NextResponse.json({ error: 'Server configuration error' }, { status: 500 });
-            }
+      const certPem = process.env.CERT_PEM;
+      if (!certPem) {
+        console.error("Error: CERT_PEM environment variable not found.");
+        return NextResponse.json(
+          { error: "Server configuration error" },
+          { status: 500 }
+        );
+      }
 
-            let key;
-            try {
-                key = await importX509(certPem, 'RS256');
-            } catch (error) {
-                console.error('Error importing certificate:', error);
-                return NextResponse.json({ error: 'Failed to import certificate' }, { status: 500 });
-            }
+      let key;
+      try {
+        key = await importX509(certPem, "RS256");
+      } catch (error) {
+        console.error("Error importing certificate:", error);
+        return NextResponse.json(
+          { error: "Failed to import certificate" },
+          { status: 500 }
+        );
+      }
 
-            let decoded;
-            try {
-                decoded = await jwtVerify(id_token, key);
-                console.log('Decoded JWT Payload:', decoded.payload); // Log the payload for debugging
-            } catch (error) {
-                console.error('Error verifying JWT:', error);
-                return NextResponse.redirect('/?error=invalid_token', { status: 302 });
-            }
+      let decoded;
+      try {
+        decoded = await jwtVerify(id_token, key);
+        console.warn("Decoded JWT Payload:", decoded.payload); // Log the payload for debugging
+      } catch (error) {
+        console.error("Error verifying JWT:", error);
+        return NextResponse.redirect("/?error=invalid_token", { status: 302 });
+      }
 
-            const email = decoded.payload.email as string;
-            if (!email) {
-                console.error('Error: Email not found in decoded token.');
-                return NextResponse.redirect('/?error=email_not_found', { status: 302 });
-            }
+      const email = decoded.payload.email as string;
+      if (!email) {
+        console.error("Error: Email not found in decoded token.");
+        return NextResponse.redirect("/?error=email_not_found", {
+          status: 302,
+        });
+      }
 
-            const user = await prisma.user.findUnique({
-                where: { email },
-            });
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
 
-            if (!user) {
-                console.log(`User with email ${email} not found in the database. Redirecting to logout.`);
-                const logoutUrl = logoutRedirectUrl(id_token);
-                const ret = NextResponse.redirect(logoutUrl, { status: 302 });
-                ret.cookies.set('cvtoken', '');
-                ret.cookies.set('cvuser', '');
-                return ret;
-            }
+      if (!user) {
+        console.error(
+          `User with email ${email} not found in the database. Redirecting to logout.`
+        );
+        const logoutUrl = logoutRedirectUrl(id_token);
+        const ret = NextResponse.redirect(logoutUrl, { status: 302 });
+        ret.cookies.set("cvtoken", "");
+        ret.cookies.set("cvuser", "");
+        return ret;
+      }
 
-            let redirectTo = `/Dashboard`;
-            if (user.isAdmin) {
-                redirectTo = '/Dashboard/dashboardEmployee/rides/page';
-            }
+      let redirectTo = `/Dashboard`;
+      if (user.isAdmin) {
+        redirectTo = "/Dashboard/dashboardEmployee/rides/page";
+      }
 
-            const maxAge = 60 * 60 * 24;
-            const redirectResponse = NextResponse.redirect(`http://localhost:3000${redirectTo}`, { status: 302 });
-            redirectResponse.cookies.set({
-                name: 'cvtoken',
-                value: id_token,
-                httpOnly: true,
-                path: '/',
-                maxAge: maxAge,
-                secure: false, // Remember to set to true in production
-            });
-            redirectResponse.cookies.set('cvuser', `${user.id}`);
-            console.log(`User ${user.id} logged in. Redirecting to: ${redirectTo}`);
-            return redirectResponse;
-
-        } catch (error) {
-            console.error('Error during callback processing:', error);
-            return NextResponse.redirect('/?error=callback_error', { status: 302 });
+      const maxAge = 60 * 60 * 24;
+      const redirectResponse = NextResponse.redirect(
+        `http://localhost:3000${redirectTo}`,
+        {
+          status: 302,
         }
+      );
+      redirectResponse.cookies.set({
+        name: "cvtoken",
+        value: id_token,
+        httpOnly: true,
+        path: "/",
+        maxAge: maxAge,
+        secure: false, // Remember to set to true in production
+      });
+      redirectResponse.cookies.set("cvuser", `${user.id}`);
+      console.warn(`User ${user.id} logged in. Redirecting to: ${redirectTo}`);
+      return redirectResponse;
+    } catch (error) {
+      console.error("Error during callback processing:", error);
+      return NextResponse.redirect("/?error=callback_error", { status: 302 });
     }
+  }
 
-    return NextResponse.json({ error: 'Invalid auth0 action' }, { status: 400 });
+  return NextResponse.json({ error: "Invalid auth0 action" }, { status: 400 });
 }
