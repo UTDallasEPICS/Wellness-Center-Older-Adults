0e3a293e4586d593a99d372140eac561ec63d47c
Deethya Janjanam <djanjanam@gmail.com>
2025-10-11 00:01:55 -0500

    everything looks good and it works well! but the status is not showing up

--- app/components/AddClientsTable.jsx
+++ app/components/AddClientsTable.jsx
@@ -5,10 +5,6 @@ import ReadOnlyClientRow from './ReadOnlyClientRow';
 const AddClientsTable = ({ customers, handleDeleteClick }) => {
     return (
         <div className="mt-[5%] ml-[calc(5%-20px)] w-[90%] text-left rounded-lg border border-gray-300 p-6 bg-white">
-            <h2 className="text-center text-[1.2rem] font-light text-gray-500 mt-4 mb-2">
-                Clients
-            </h2>
-            <div className="border-b-[3px] border-gray-600 w-[20%] mx-auto mb-2 mt-[-10px]"></div>
 
             <table className="border-collapse w-full">
                 <thead>
